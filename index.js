a77b79d70fb6f8310c76148cccc50d442e2998fd
Kunal Bhat <kunal.bhat2001@gmail.com>
2022-04-05 09:58:45 +0530

    Remove native server

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const { createEventAdapter, errorCodes } = require("@slack/events-api");
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8080;
 
 const SLACK_SIGNING_SECRET = process.env.SLACK_SIGNING_SECRET;
+
 const slackEvents = createEventAdapter(SLACK_SIGNING_SECRET, {
   includeBody: true,
   includeHeaders: true
@@ -8,4 +13,12 @@ const slackEvents = createEventAdapter(SLACK_SIGNING_SECRET, {
 
 slackEvents.on("reaction_added", (ev, body)=>{
     if(body.reaction=="thumbsup") console.log("Upped thumbs !?!");
+});
+
+app.use("/events/api/", slackEvents.requestListener());
+
+app.use(express.json())
+
+app.listen(PORT, ()=>{
+    console.log(`k8s community bot running on ${PORT}`);
 })
